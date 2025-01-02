78fc6ba282ec7b327e0ee26ee8f83c84d172c4c5
fatin-israq <israqq2120@gmail.com>
2025-01-02 13:47:24 +0600

    Added FXML fields in the Controller

--- src/main/java/com/example/bankify/Controllers/loginController.java
+++ src/main/java/com/example/bankify/Controllers/loginController.java
@@ -1,5 +1,15 @@
 package com.example.bankify.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class loginController {
 
+    public Label payee_address_lbl;
+    public TextField payee_address_fld;
+    public PasswordField password_fld;
+    public Button login_btn;
+    public Label error_lbl;
 }
