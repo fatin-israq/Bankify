5a1835e8fdc5636f47cbfe2bf6cfd92aa9cc3d01
fatin-israq <israqq2120@gmail.com>
2025-01-05 21:34:07 +0600

    Add method onLogin()

--- src/main/java/com/example/bankify/Controllers/loginController.java
+++ src/main/java/com/example/bankify/Controllers/loginController.java
@@ -1,15 +1,30 @@
 package com.example.bankify.Controllers;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import com.example.bankify.Models.Model;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
-public class loginController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class loginController implements Initializable {
+
+    public ChoiceBox acc_selector;
     public Label payee_address_lbl;
     public TextField payee_address_fld;
     public PasswordField password_fld;
     public Button login_btn;
     public Label error_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        login_btn.setOnAction(actionEvent -> onLogin());
+    }
+
+    private void onLogin() {
+        Stage stage = (Stage) error_lbl.getScene().getWindow();
+        Model.getInstance().getViewFactory().closeStage(stage);
+        Model.getInstance().getViewFactory().showClientWindow();
+    }
 }
