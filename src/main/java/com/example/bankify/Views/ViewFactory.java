752c1267c87d493d8162d1631c5b5053d0ea03d7
fatin-israq <israqq2120@gmail.com>
2025-01-05 21:35:57 +0600

    Update ViewFactory

--- src/main/java/com/example/bankify/Views/ViewFactory.java
+++ src/main/java/com/example/bankify/Views/ViewFactory.java
@@ -1,4 +1,91 @@
 package com.example.bankify.Views;
 
+import com.example.bankify.Controllers.Client.ClientController;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class ViewFactory {
+    // Client Views
+    private final StringProperty clientSelectedMenuItem;
+    private AnchorPane dashboardView;
+    private AnchorPane transactionsView;
+    private AnchorPane accountsView;
+
+    public ViewFactory(){
+        this.clientSelectedMenuItem = new SimpleStringProperty("");
+    }
+
+    /*
+    * Client View Section
+    * */
+    public StringProperty getClientSelectedMenuItem() {
+        return clientSelectedMenuItem;
+    }
+
+    public AnchorPane getDashboardView() {
+            if ((dashboardView == null)){
+                try {
+                    dashboardView = new FXMLLoader(getClass().getResource("/Fxml/Client/Dashboard.fxml")).load();
+                } catch (Exception e){
+                    e.printStackTrace();
+            }
+        }
+            return dashboardView;
+    }
+
+    public AnchorPane getTransactionsView() {
+        if((transactionsView == null)){
+            try {
+                transactionsView = new FXMLLoader(getClass().getResource("/Fxml/Client/Transactions.fxml")).load();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return transactionsView;
+    }
+
+    public AnchorPane getAccountsView() {
+        if((accountsView == null)){
+            try {
+                accountsView = new FXMLLoader(getClass().getResource("/Fxml/Client/Accounts.fxml")).load();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return accountsView;
+    }
+
+    public void showLoginWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+        createStage(loader);
+    }
+
+    public void showClientWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Client/Client.fxml"));
+        ClientController clientController = new ClientController();
+        loader.setController(clientController);
+        createStage(loader);
+
+    }
+    public void createStage(FXMLLoader loader) {
+        Scene scene = null;
+        try {
+            scene = new Scene(loader.load());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Bankify");
+        stage.show();
+    }
+
+    public void closeStage(Stage stage) {
+        stage.close();
+    }
 }
