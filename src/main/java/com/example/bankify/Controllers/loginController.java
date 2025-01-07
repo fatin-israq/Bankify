d692bba3202e0d751671b251e500fbb0997dae16
fatin-israq <israqq2120@gmail.com>
2025-01-08 01:27:54 +0600

    update onLogin and initialize methods

--- src/main/java/com/example/bankify/Controllers/loginController.java
+++ src/main/java/com/example/bankify/Controllers/loginController.java
@@ -1,6 +1,8 @@
 package com.example.bankify.Controllers;
 
 import com.example.bankify.Models.Model;
+import com.example.bankify.Views.AccountType;
+import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
@@ -10,7 +12,7 @@ import java.util.ResourceBundle;
 
 public class loginController implements Initializable {
 
-    public ChoiceBox acc_selector;
+    public ChoiceBox<AccountType> acc_selector;
     public Label payee_address_lbl;
     public TextField payee_address_fld;
     public PasswordField password_fld;
@@ -19,12 +21,20 @@ public class loginController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        login_btn.setOnAction(actionEvent -> onLogin());
+        acc_selector.setItems(FXCollections.observableArrayList(AccountType.CLIENT, AccountType.ADMIN));
+        acc_selector.setValue(Model.getInstance().getViewFactory().getLoginAccountType());
+        acc_selector.valueProperty().addListener(observable->Model.getInstance().getViewFactory().setLoginAccountType(acc_selector.getValue()));
+        login_btn.setOnAction(event -> onLogin());
     }
 
     private void onLogin() {
         Stage stage = (Stage) error_lbl.getScene().getWindow();
         Model.getInstance().getViewFactory().closeStage(stage);
-        Model.getInstance().getViewFactory().showClientWindow();
+        if(Model.getInstance().getViewFactory().getLoginAccountType()==AccountType.CLIENT) {
+            Model.getInstance().getViewFactory().showClientWindow();
+        }
+        else {
+            Model.getInstance().getViewFactory().showAdminWindow();
+        }
     }
 }
