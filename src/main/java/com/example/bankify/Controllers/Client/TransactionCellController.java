617dd98eaf9ee38a10006a1f4e34eec40927c9d9
Borno <sadekinborno07@gmail.com>
2025-01-13 23:26:56 +0600

    Controller for TransactionCell in Client folder

---
+++ src/main/java/com/example/bankify/Controllers/Client/TransactionCellController.java
@@ -0,0 +1,24 @@
+package com.example.bankify.Controllers.Client;
+
+import com.example.bankify.Models.Transaction;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class TransactionCellController implements Initializable {
+    public FontAwesomeIconView in_icon;
+    public FontAwesomeIconView out_icon;
+    public Label trans_date_lbl;
+    public Label sender_lbl;
+    public Label receiver_lbl;
+    public Label amount_lbl;
+
+    private final Transaction transaction;
+
+    public TransactionCellController(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    @Override
+    public void initialize(java.net.URL url, java.util.ResourceBundle resourceBundle) {}
+}
