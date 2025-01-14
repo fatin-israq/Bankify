e7696c4d1aeb48dfa8ebc33a384f11583037fc83
fatin-israq <israqq2120@gmail.com>
2025-01-14 19:52:41 +0600

    Add client and admin data handling in Model class
    Introduced new fields and methods to manage client and admin data in the `Model` class. This includes login success flags, account type management, and client credential evaluation functionality integrated with the database. Enhanced the `Model` to improve data encapsulation and interaction.

--- src/main/java/com/example/bankify/Models/Model.java
+++ src/main/java/com/example/bankify/Models/Model.java
@@ -1,14 +1,30 @@
 package com.example.bankify.Models;
 
+import com.example.bankify.Views.AccountType;
 import com.example.bankify.Views.ViewFactory;
+
+import java.sql.ResultSet;
+import java.time.LocalDate;
 
 public class Model {
     private static Model model;
     private final ViewFactory viewFactory;
+    private final DatabaseDriver databaseDriver;
+    private AccountType loginAccountType = AccountType.CLIENT;  // Initially it is client
+    // Client Datq Section
+    private final Client client;
+    private boolean clientLoginSuccessFlag;
+    // Admin Data Section
+
 
     private Model() {
         this.viewFactory = new ViewFactory();
+        databaseDriver = new DatabaseDriver();
+        // Client Data Section
+        this.clientLoginSuccessFlag = false;
+        this.client = new Client("", "", "", null, null, null);
 
+        //Admin Data Section
     }
 
     public static synchronized Model getInstance() {
@@ -20,5 +36,51 @@ public class Model {
 
     public ViewFactory getViewFactory() {
         return viewFactory;
+    }
+
+    public DatabaseDriver getDatabaseDriver() {
+        return databaseDriver;
+    }
+
+    public AccountType getLoginAccountType() {
+        return this.loginAccountType;
+    }
+
+    public void setLoginAccountType(AccountType loginAccountType) {
+        this.loginAccountType = loginAccountType;
+    }
+
+    /*
+        Client Method Section
+         */
+    public boolean getClientLoginSuccessFlag() {
+        return this.clientLoginSuccessFlag;
+    }
+
+    public void setClientLoginSuccessFlag(boolean flag) {
+        this.clientLoginSuccessFlag = flag;
+    }
+
+    public Client getClient() {
+        return this.client;
+    }
+
+    public void evaluateClientCred(String pAddress, String password){
+        CheckingAccount checkingAccount;
+        SavingsAccount savingsAccount;
+        ResultSet resultSet = databaseDriver.getClientData(pAddress, password);
+        try {
+            if (resultSet.isBeforeFirst()) {
+                this.client.firstNameProperty().set(resultSet.getString("FirstName"));
+                this .client.lastNameProperty().set(resultSet.getString("LastName"));
+                this.client.pAddressProperty().set(resultSet.getString("PayeeAddress"));
+                String[] dateParts = resultSet.getString("Date").split("-");
+                LocalDate date = LocalDate.of(Integer.parseInt(dateParts[0]), Integer.parseInt(dateParts[1]), Integer.parseInt(dateParts[2]));
+                this.client.dateProperty().set(date);
+                this.clientLoginSuccessFlag = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
