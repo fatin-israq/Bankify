b5dd63e7398a23a327f45cda146184495ea3aee1
fatin-israq <israqq2120@gmail.com>
2025-01-14 18:13:04 +0600

    Add SavingsAccount model with withdrawal limit property
    Introduces a new SavingsAccount class extending Account. This class includes a withdrawal limit property to manage restrictions on withdrawals for savings accounts. It provides a constructor and a property accessor for the withdrawal limit.

---
+++ src/main/java/com/example/bankify/Models/SavingsAccount.java
@@ -0,0 +1,19 @@
+package com.example.bankify.Models;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class SavingsAccount extends Account{
+    // The withdraw limit from the savings
+    private final DoubleProperty withdrawalLimit;
+
+    public SavingsAccount(String owner, String accountNumber, double balance, double withdrawalLimit) {
+        super(owner, accountNumber, balance);
+        this.withdrawalLimit = new SimpleDoubleProperty(this, "Withdrawal Limit", withdrawalLimit);
+    }
+
+    public DoubleProperty withdrawalLimitProp() {
+        return withdrawalLimit;
+    }
+
+}
