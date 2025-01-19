f1ee6702fe42b0d0743589745496a0a75c731916
MJahanEna <mjahanena1@gmail.com>
2025-01-19 13:22:57 +0600

    Worked on Logout button. Also added deposit button

--- src/main/java/com/example/bankify/Controllers/Admin/AdminMenuController.java
+++ src/main/java/com/example/bankify/Controllers/Admin/AdminMenuController.java
@@ -4,6 +4,7 @@ import com.example.bankify.Models.Model;
 import com.example.bankify.Views.AdminMenuOptions;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,6 +23,8 @@ public class AdminMenuController implements Initializable {
     private void addListeners(){
         create_client_btn.setOnAction(e -> onCreateClient());
         clients_btn.setOnAction(e -> onClients());
+        deposit_btn.setOnAction(e -> onDeposit());
+        logout_btn.setOnAction(e -> onLogout());
     }
 
     private void onCreateClient(){
@@ -30,5 +33,20 @@ public class AdminMenuController implements Initializable {
 
     private void onClients(){
         Model.getInstance().getViewFactory().getAdminSelectedMenuItem().set(AdminMenuOptions.CLIENTS);
+    }
+
+    private void onDeposit(){
+        Model.getInstance().getViewFactory().getAdminSelectedMenuItem().set(AdminMenuOptions.DEPOSIT);
+    }
+
+    private void onLogout(){
+        //Get Stage
+        Stage stage = (Stage) clients_btn.getScene().getWindow();
+        //Close the Admin window
+        Model.getInstance().getViewFactory().closeStage(stage);
+        //Show Login Window
+        Model.getInstance().getViewFactory().showLoginWindow();
+        //Set Admin Login Success Flag To False
+        Model.getInstance().setAdminLoginSuccessFlag(false);
     }
 }
