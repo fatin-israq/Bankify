cda7161f69119b242334c83428bbc75349e670be
fatin-israq <israqq2120@gmail.com>
2025-01-20 14:26:59 +0600

    Add toString method

--- src/main/java/com/example/bankify/Models/CheckingAccount.java
+++ src/main/java/com/example/bankify/Models/CheckingAccount.java
@@ -7,12 +7,17 @@ public class CheckingAccount extends Account {
     // The number of transactions a client is allowed to do per day.
     private final IntegerProperty transactionLimit;
 
-    public CheckingAccount(String owner, String accountNumber, double balance, int tlimit) {
+    public CheckingAccount(String owner, String accountNumber, double balance, int tLimit) {
         super(owner, accountNumber, balance);
-        this.transactionLimit = new SimpleIntegerProperty(this, "Transaction Limit", tlimit);
+        this.transactionLimit = new SimpleIntegerProperty(this, "Transaction Limit", tLimit);
     }
 
     public IntegerProperty transactionLimitProp() {
         return transactionLimit;
+    }
+
+    @Override
+    public String toString() {
+        return accountNumberProperty().get();
     }
 }
