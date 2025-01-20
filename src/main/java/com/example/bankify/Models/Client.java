0ca33a018a7b111e3af1f9f697731e82423e6cf1
fatin-israq <israqq2120@gmail.com>
2025-01-20 14:28:10 +0600

    Refactor misspelled variables.

--- src/main/java/com/example/bankify/Models/Client.java
+++ src/main/java/com/example/bankify/Models/Client.java
@@ -11,7 +11,7 @@ public class Client {
     private final StringProperty firstName;
     private final StringProperty lastName;
     private final StringProperty payeeAddress;
-    private final ObjectProperty<Account> chekingAccount;
+    private final ObjectProperty<Account> checkingAccount;
     private final ObjectProperty<Account> savingsAccount;
     private final ObjectProperty<LocalDate> dateCreated;
 
@@ -19,7 +19,7 @@ public class Client {
         this.firstName = new SimpleStringProperty(this, "FirstName", fName);
         this.lastName = new SimpleStringProperty(this, "LastName", lName);
         this.payeeAddress = new SimpleStringProperty(this, "Payee Address", pAddress);
-        this.chekingAccount = new SimpleObjectProperty<>(this, "Checking Account", cAccount);
+        this.checkingAccount = new SimpleObjectProperty<>(this, "Checking Account", cAccount);
         this.savingsAccount = new SimpleObjectProperty<>(this, "Saving Account", sAccount);
         this.dateCreated = new SimpleObjectProperty<>(this, "Date Created", date);
     }
@@ -27,7 +27,7 @@ public class Client {
     public StringProperty firstNameProperty() {return firstName;}
     public StringProperty lastNameProperty() {return lastName;}
     public StringProperty pAddressProperty() {return payeeAddress;}
-    public ObjectProperty<Account> chekingAccountProperty() {return chekingAccount;}
+    public ObjectProperty<Account> checkingAccountProperty() {return checkingAccount;}
     public ObjectProperty<Account> savingsAccountProperty() {return savingsAccount;}
     public ObjectProperty<LocalDate> dateProperty() {return dateCreated;}
 
