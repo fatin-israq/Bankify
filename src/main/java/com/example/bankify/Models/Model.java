ba910ee597d62bc9405a39d248e8b24a5640fc3a
fatin-israq <israqq2120@gmail.com>
2025-01-20 14:35:02 +0600

    Enhance Model class with client and account management functionality

--- src/main/java/com/example/bankify/Models/Model.java
+++ src/main/java/com/example/bankify/Models/Model.java
@@ -2,6 +2,8 @@ package com.example.bankify.Models;
 
 import com.example.bankify.Views.AccountType;
 import com.example.bankify.Views.ViewFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.sql.ResultSet;
 import java.time.LocalDate;
@@ -15,6 +17,7 @@ public class Model {
     private boolean clientLoginSuccessFlag;
     // Admin Data Section
     private boolean adminLoginSuccessFlag;
+    private final ObservableList<Client> clients;
 
     private Model() {
         this.viewFactory = new ViewFactory();
@@ -25,6 +28,7 @@ public class Model {
 
         //Admin Data Section
         this.adminLoginSuccessFlag = false;
+        this.clients = FXCollections.observableArrayList();
     }
 
     public static synchronized Model getInstance() {
@@ -69,6 +73,10 @@ public class Model {
                 String[] dateParts = resultSet.getString("Date").split("-");
                 LocalDate date = LocalDate.of(Integer.parseInt(dateParts[0]), Integer.parseInt(dateParts[1]), Integer.parseInt(dateParts[2]));
                 this.client.dateProperty().set(date);
+                checkingAccount = getCheckingAccount(pAddress);
+                savingsAccount = getSavingsAccount(pAddress);
+                this.client.checkingAccountProperty().set(checkingAccount);
+                this.client.savingsAccountProperty().set(savingsAccount);
                 this.clientLoginSuccessFlag = true;
             }
         } catch (Exception e) {
@@ -97,4 +105,60 @@ public class Model {
             e.printStackTrace();
         }
     }
+
+    public ObservableList<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients() {
+        CheckingAccount checkingAccount;
+        SavingsAccount savingsAccount;
+        ResultSet resultSet = databaseDriver.getAllClientsData();
+        try{
+            while (resultSet.next()) {
+                String fName = resultSet.getString("FirstName");
+                String lName = resultSet.getString("LastName");
+                String pAddress = resultSet.getString("PayeeAddress");
+                String[] dateParts = resultSet.getString("Date").split("-");
+                LocalDate date = LocalDate.of(Integer.parseInt(dateParts[0]), Integer.parseInt(dateParts[1]), Integer.parseInt(dateParts[2]));
+                checkingAccount = getCheckingAccount(pAddress);
+                savingsAccount = getSavingsAccount(pAddress);
+                clients.add(new Client(fName, lName, pAddress, checkingAccount, savingsAccount, date));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    * Utility Methods Section
+    * */
+    public CheckingAccount getCheckingAccount(String pAddress) {
+        CheckingAccount account = null;
+        ResultSet resultSet = databaseDriver.getCheckingAccountData(pAddress);
+        try {
+            String num = resultSet.getString("AccountNumber");
+            int tLimit = (int) resultSet.getDouble("TransactionLimit");
+            double balance = resultSet.getDouble("Balance");
+            account = new CheckingAccount(pAddress, num, balance, tLimit);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return account;
+    }
+
+    public SavingsAccount getSavingsAccount(String pAddress) {
+        SavingsAccount account = null;
+        ResultSet resultSet = databaseDriver.getSavingsAccountData(pAddress);
+        try {
+            String num = resultSet.getString("AccountNumber");
+            double wLimit = resultSet.getDouble("WithdrawalLimit");
+            double balance = resultSet.getDouble("Balance");
+            account = new SavingsAccount(pAddress, num, balance, wLimit);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return account;
+    }
+
 }
