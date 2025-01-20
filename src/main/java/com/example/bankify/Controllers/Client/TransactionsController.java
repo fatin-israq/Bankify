8bb522a8fcda96c3ef1b0fba082eca53474ee4b2
fatin-israq <israqq2120@gmail.com>
2025-01-20 17:04:59 +0600

    Enhance TransactionsController to display transactions with custom cell rendering

--- src/main/java/com/example/bankify/Controllers/Client/TransactionsController.java
+++ src/main/java/com/example/bankify/Controllers/Client/TransactionsController.java
@@ -1,5 +1,8 @@
 package com.example.bankify.Controllers.Client;
 
+import com.example.bankify.Models.Model;
+import com.example.bankify.Models.Transaction;
+import com.example.bankify.Views.TransactionCellFactory;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
@@ -7,10 +10,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TransactionsController implements Initializable {
-    public ListView transactions_listview;
+    public ListView<Transaction> transactions_listview;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        initAllTransactionsList();
+        transactions_listview.setItems(Model.getInstance().getAllTransactions());
+        transactions_listview.setCellFactory(e -> new TransactionCellFactory());
+    }
 
+    private void initAllTransactionsList() {
+        if (Model.getInstance().getAllTransactions().isEmpty()){
+            Model.getInstance().setAllTransactions();
+        }
     }
 }
