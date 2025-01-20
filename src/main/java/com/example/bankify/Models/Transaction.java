bbc997c0dbc67d01e00a51cc96c69ce8db408e09
fatin-israq <israqq2120@gmail.com>
2025-01-20 14:39:08 +0600

    Refactor reciever to receiver.

--- src/main/java/com/example/bankify/Models/Transaction.java
+++ src/main/java/com/example/bankify/Models/Transaction.java
@@ -6,14 +6,14 @@ import java.time.LocalDate;
 
 public class Transaction {
     private final StringProperty sender;
-    private final StringProperty reciever;
+    private final StringProperty receiver;
     private final DoubleProperty amount;
     private final ObjectProperty<LocalDate> date;
     private final StringProperty message;
 
-    public Transaction(String sender, String reciever, Double amount, LocalDate date, String message) {
-        this.sender = new SimpleStringProperty(this, "sender", sender);
-        this.reciever = new SimpleStringProperty(this, "Reciever", reciever);
+    public Transaction(String sender, String receiver, Double amount, LocalDate date, String message) {
+        this.sender = new SimpleStringProperty(this, "Sender", sender);
+        this.receiver = new SimpleStringProperty(this, "Receiver", receiver);
         this.amount = new SimpleDoubleProperty(this, "Amount", amount);
         this.date = new SimpleObjectProperty(this, "Date", date);
         this.message = new SimpleStringProperty(this, "Message", message);
@@ -21,7 +21,7 @@ public class Transaction {
 
     public StringProperty senderProperty() {return this.sender;}
 
-    public StringProperty recieverProperty() {return this.reciever;}
+    public StringProperty receiverProperty() {return this.receiver;}
 
     public DoubleProperty amountProperty() {return this.amount;}
 
