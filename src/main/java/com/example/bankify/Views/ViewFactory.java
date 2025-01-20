5d3614506ac20f9e15482cba2cf3d845e6f02e24
fatin-israq <israqq2120@gmail.com>
2025-01-20 14:40:54 +0600

    Add getDeposit view for viewing Deposit section from Admin panel.

--- src/main/java/com/example/bankify/Views/ViewFactory.java
+++ src/main/java/com/example/bankify/Views/ViewFactory.java
@@ -22,6 +22,7 @@ public class ViewFactory {
     private final ObjectProperty<AdminMenuOptions> adminSelectedMenuItem;
     private AnchorPane createClientView;
     private AnchorPane clientsView;
+    private AnchorPane depositView;
 
     public ViewFactory(){
         this.loginAccountType = AccountType.CLIENT;
@@ -112,6 +113,17 @@ public class ViewFactory {
             }
         }
         return clientsView;
+    }
+
+    public AnchorPane getDepositView() {
+        if (depositView == null){
+            try {
+                depositView = new FXMLLoader(getClass().getResource("/Fxml/Admin/Deposit.fxml")).load();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return depositView;
     }
 
     public void showAdminWindow() {
