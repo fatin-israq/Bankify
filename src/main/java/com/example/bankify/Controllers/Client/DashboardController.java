546aafe0eb70e1843d05892f97bac043fc5affc8
fatin-israq <israqq2120@gmail.com>
2025-01-20 16:20:21 +0600

    Bind data so that UserDashboard can retrieve the Users name and Show it on screen.

--- src/main/java/com/example/bankify/Controllers/Client/DashboardController.java
+++ src/main/java/com/example/bankify/Controllers/Client/DashboardController.java
@@ -1,10 +1,13 @@
 package com.example.bankify.Controllers.Client;
 
+import com.example.bankify.Models.Model;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
@@ -23,5 +26,16 @@ public class DashboardController implements Initializable {
     public Button send_money_btn;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle){}
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        bindData();
+    }
+
+    private void bindData(){
+        user_name.textProperty().bind(Bindings.concat("Hi, ").concat(Model.getInstance().getClient().firstNameProperty()));
+        login_date.setText("Today, " + LocalDate.now());
+        checking_bal.textProperty().bind(Model.getInstance().getClient().checkingAccountProperty().get().balanceProperty().asString());
+        checking_acc_num.textProperty().bind(Model.getInstance().getClient().checkingAccountProperty().get().accountNumberProperty());
+        savings_bal.textProperty().bind(Model.getInstance().getClient().savingsAccountProperty().get().balanceProperty().asString());
+        savings_acc_num.textProperty().bind(Model.getInstance().getClient().savingsAccountProperty().get().accountNumberProperty());
+    }
 }
